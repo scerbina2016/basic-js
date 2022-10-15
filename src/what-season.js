b01017b2ce15b8345b5429133cdeec42f0d244ab
Andrey Scerbina <scerbina2016@gmail.com>
2022-10-15 12:29:17 +0300

    refactor: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+//let dateIn=() => new Date();//new Date(2150, 7, 21, 18, 36, 41, 841);
 
+function getSeason(date) {
+  //console.log(Object.prototype.toString.call(date));
+  let season='Invalid date!';
+  if(date==null || date==undefined){return 'Unable to determine the time of year!'}
+  if((Object.prototype.toString.call(date)==='[object Date]')){  //==================
+  //console.log('in circle');
+    
+ let month=date.getMonth();
+ let day=date.getDay();
+ 
+ 
+ if(month==11){season='winter'};
+if (month>='0'&& month<='1') {season='winter'};
+if (month>='2'&& month<='4') {season='spring'};
+if (month>='5'&& month<='7') {season='summer'};
+if (month>='8'&& month<='10') {season='autumn'}; 
+//console.log(month);
+//console.log(date);
+} else {return 'Invalid date!'};//======================
+  return season;
+ }
+ 
+//console.log(getSeason(dateIn));
 module.exports = {
   getSeason
 };
